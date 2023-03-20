ebef2567f0f5532fb147bd35292a5942808b796d
Alenago911 <alena.chernyavka@gmail.com>
2023-03-20 17:56:03 +0300

    task 1 20.03

---
+++ js/task-01.js
@@ -0,0 +1,19 @@
+const navEl = document.querySelectorAll('.item');
+console.log(navEl);
+
+
+const NumberOfCategories = navEl.length;
+console.log(NumberOfCategories);
+
+
+navEl.forEach ((element) => {
+console.log(element)
+console.log(element.firstElementChild)
+const title = element.querySelector("h2").textContent;
+console.log(title);
+const quantityEl = element.querySelectorAll("li").length;
+console.log(quantityEl);
+
+});
+
+
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+// const ingredients = [
+//   'Potatoes',
+//   'Mushrooms',
+//   'Garlic',
+//   'Tomatos',
+//   'Herbs',
+//   'Condiments',
+// ];
+
+
+// const navItem =  document.createElement('li');
+// navItem.classList.add('item');
+// navItem.textContent = ('Potatoes')
+// console.log(navItem);
+
+// ingredients.appendChild(navItem);
+// console.log(ingredients)
