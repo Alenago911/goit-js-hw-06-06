b5f454eb8d331dc11910d1d74af8ecfb8a48cae7
Alenago911 <alena.chernyavka@gmail.com>
2023-03-27 12:36:51 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -6,11 +6,6 @@ function getRandomHexColor() {
 
 let hex = getRandomHexColor();
 
-function getRandomHexColor() {
-return `#${Math.floor(Math.random() * 16777215)
-.toString(16)
-.padStart(6, 0)}`;
-}
 const backgroundColor = document.querySelector(".color");
 const button = document.querySelector(".change-color");
 button.addEventListener("click", function onClick(event) {
